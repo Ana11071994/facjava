d2cefbeb9ad80ecd21956397ec8cb31b6a7dc6c8
Ana11071994 <anand11mech@gmail.com>
2023-04-15 16:14:09 +0530

    verb

---
+++ fac.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		int a =8;
+		int fact=1;
+		for(int i=1;i<=a;i++){
+		fact=fact*i;
+	}
+System.out.println(fact);
+}
+}
